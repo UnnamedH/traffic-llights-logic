01f4476f8dda1cd0db11554239a376aa5e14163a
UnnamedCat_ <47675080+UnnamedH@users.noreply.github.com>
2025-05-06 14:00:14 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+TWILIO_ACCOUNT_SID = 'your_twilio_sid'
+TWILIO_AUTH_TOKEN = 'your_twilio_auth'
+
+TWILIO_WHATSAPP_NUMBER = 'whatsapp:+[twilioNumber]'
+DESTINATION_WHATSAPP_NUMBER = 'whatsapp:+[yourNumber]'
+
+GCS_BUCKET_NAME = 'your_bucket_name'
+GCS_PROJECT_ID = 'your_project_id'
+
+GCS_CREDENTIALS_PATH = '/path/to/credentials.json'
+
+GCS_BASE_URL = 'https://your.bucket.path'
+
+PIN_1 = 18
+PIN_2 = 23
+
+CAMERA_RESOLUTION = (1296, 972)
+IMAGE_FOLDER = '/home/pi/camera_snapshots'
+
+COOLDOWN_PERIOD = 1
+NOTIFICATION_MESSAGE = "Alert! Someone passed a red light! @ "
